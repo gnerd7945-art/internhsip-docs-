4ddd16247cdb39b856041e1eaaa496f602347d2a
gnerd7945-art <gnerd7945@gmail.com>
2026-03-23 09:11:17 +0530

    Update is_a_has_a_factory.cpp

--- cpp/is_a_has_a_factory.cpp
+++ cpp/is_a_has_a_factory.cpp
@@ -31,7 +31,7 @@ public:
    engine = new Engine(p,engine_name); // used in constructor
  };
 void specs(){
-  std::cout<<"my specs:" <<name<<","<<load<<","<<engine->get_power()<<","<<engine->get_type()<<std::endl;
+  std::cout<<"my specs:" <<name<<","<<load<<","<<engine->get_power()<<","<<engine->get_type()<<std::endl; // cant acess engine->PowerValue and engine->Engine_type directly as they are private. 
 }
 void change_engine(int power,string name){
   engine->set_power(power); // here input params "power", setter function has 'p'
@@ -51,7 +51,7 @@ public:
    engine = new Engine(p,engine_name);
  };
 void specs(){
-  std::cout<<"my specs:" <<name<<","<<load<<","<<engine->power<<","<<engine->type<<std::endl;
+  std::cout<<"my specs:" <<name<<","<<load<<","<<engine->get_power()<<","<<engine->get_type()<<std::endl;
 }
 void change_engine(int power,string name){
   engine->set_power(power); // here input params "power", setter function has 'p'
@@ -70,8 +70,67 @@ return 0;
 }
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
   **some notes, notice that tuck and ship both are vehivle and could be inherited from a base class, named Vehicle. 
-  this will help us prune redundunt variables, , you have name, load, and Engine* engine declared in both classes. If you decide later that every vehicle also needs a 
+  this will help us prune redundant variables, , you have name, load, and Engine* engine declared in both classes. If you decide later that every vehicle also needs a 
 fuelLevel, you’d have to add it twice. With a base class, you declare these once in Vehicle. same for specs() method**
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+ class Engine{
+private:
+int PowerValue;
+string Engine_type;
+public:
+   Engine(int power, string name):PowerValue(power),type(name){};
+int get_power(){
+  return PowerValue;
+}
+string get_type(){
+  return type;
+}
+//setter
+void set_power(int p){
+  PowerValue = p;
+}
+void set_engine_type(string name){
+  Engine_type = name;
+}
+};
+class Vehicle{
+private:
+string name;
+int load;
+Engine* engine; 
+public: 
+Vehicle(string name, int load, int PowerValue, string Enigne_Type): name(name),load(load){
+  engine = new Engine(PowerValue,Engine_Type);}
+void specs(){
+    std::cout<<"my specs:" <<name<<","<<load<<","<<engine->get_power()<<","<<engine->get_type()<<std::endl;
+}
+void change_engine(int Power, string name){
+  engine->set_power(Power);
+  engine->set_engine_type(name);
+}
+~Vehicle(){
+  delete engine;
+}   
+};
+class Truck :: public Vehicle{
+public:
+ Truck(string name, int load, int PowerValue, string Enigne_Type): Vehicle(name, load,PowerValue,Engine_Type){ // vehicle constructor called. 
+   std::cout<<"TRuck created"<<std::endl;
+     }
+};
+class Ship{
+ public:
+Ship{
+using Vehicle::Vehicle; // tells Shipe to use Vehicle constructor
+};
+int main(){
+  Truck truck( "tata",500,2000,"tata_enigne);
+ truck.specs();
+ truck.change_engine(3000,"updated_tata_engine");
+ truck.specs();
+return 0;
+}
 
   
 
