109540080b07cadc5a1e17c4e0050eba58aebe6d
gnerd7945-art <gnerd7945@gmail.com>
2026-05-04 21:50:11 +0530

    Update syntax.cpp

--- cpp/syntax.cpp
+++ cpp/syntax.cpp
@@ -250,7 +250,7 @@ int main() {
 2) variable = (condition) ? value_if_true : value_if_false; // not == btw variable and condition 
 3) if in param you do: const class_name& obj. Then inside that function(which uses that params)  you can only call const function  of that obj, func() const allow func() to be called if obj const 
 4) almost always if return type  is private reference, make it const in function declaration or make it const if function is const, return by value, then const not needed. 
-5) unordered_map can't be sorted. 
+5) unordered_map can't be sorted.Convert it to vector of pairs and sort them.  
 
 
 
