d1c93fd819b862d6daebc4d64692cfa5482245a3
gnerd7945-art <gnerd7945@gmail.com>
2026-07-03 15:28:46 +0530

    Create cool_syntax.cpp

---
+++ cpp/cool_syntax.cpp
@@ -0,0 +1,29 @@
+struct PublishProfile 
+{
+    std::string name;
+    std::string cameraTransport;
+    std::string ffmpegRtspTransport;
+    std::string protocol;
+};
+PublishProfile profile{       // initalize: c20 feature , ordering matter 
+        .name = "rtmp_tcp",
+        .cameraTransport = "tcp",
+        .ffmpegRtspTransport = "tcp",
+        .protocol = "rtmp",
+    };
+
+PublishProfile profile{ // traditional way, ordering matter 
+    "rtmp_tcp", 
+    "tcp", 
+    "tcp", 
+    "rtmp"
+};
+
+// 1. Create an empty struct (variables default to empty strings)
+PublishProfile profile; 
+
+// 2. Assign values one by one (Order does not matter here!)
+profile.protocol = "rtmp";
+profile.name = "rtmp_tcp";
+profile.cameraTransport = "tcp";
+profile.ffmpegRtspTransport = "tcp";
