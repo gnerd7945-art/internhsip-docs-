6754bfdef3c5d356fec1dd8937096e0ee1e7fff4
gnerd7945-art <gnerd7945@gmail.com>
2026-08-21 14:30:26 +0530

    Update smart_pointers.cpp

--- cpp/smart_pointers.cpp
+++ cpp/smart_pointers.cpp
@@ -1,4 +1,5 @@
-// use to avoid memory leak via early returns or throw, or maybe simply forgetting to use delete. 
+// Since mem on heap is not cleared until delete is called ( which calls there destrcutor first before freeing mem)  we use  smart pointers to avoid memory leak which could happen via early returns or throw, 
+or maybe simply forgetting to use delete.  the destructor is not called automatically when a pointer goes out of scope when mem is allocated on heap. We need to amage it via delete or smart pointers. 
 
 // smart pointers are allocated on stack and when they go out of scope, they call there destructor which have delete in there code to remove memory of object.
 
@@ -7,6 +8,6 @@
 // although mem allocation is with unique pointer, order is not always l-r: process_data(std::unique_ptr<Document>(new Document()), get_priority_from_server());
 // here new Document ahppended first, get_priority_from_server() gets called ( failed, thus return) cannot execute unique_ptr argument. 
 // make unique is a single function call. it allocates memory and immediately calls unique pointer. 
-
-// new int[n] => returns pointer(int*) ,     int * p = new int(10); // single element 
+// example intialization:     std::unique_ptr<Player> p2 = std::make_unique<Player>("Bob", 200); , std::unique_ptr<Player> p3(new Player("Charlie", 300));
+//  int * p = new int(10); // single element 
 
